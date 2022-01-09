b72abb918a441afcdb8d2bc3eec871e9615075d4
alexanderrevotiuk <revotiukalexander@gmail.com>
2022-01-09 16:24:14 +0300

    Complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+    let str = String(Math.abs(n));
+    for (let i = str.length; i > 0; i--) {
+      result = result + str[i-1];
+    }
+    return Number(result);
 }
